ee5b2fa178a9764f0f961c23a1825f5ec7821419
Igor <igor-skromnik@yandex.ru>
2023-05-24 00:49:29 +0300

    fix-changes-on-test-page
    - update test page
    - add custom bar to main page and theme control

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:clippy/pages/home/home.dart';
+import 'package:clippy/pages/user/user_profile.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,25 +10,61 @@ void test() {
 
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
 
+  static _MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>()!;
+
+  const MyApp({super.key});
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  ThemeMode _themeMode = ThemeMode.system;
+
+  void changeTheme(ThemeMode themeMode) {
+    setState(() {
+      _themeMode = themeMode;
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purpleAccent),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
         useMaterial3: true,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero),
+                minimumSize: const Size(300, 50)
+            )
+        )
       ),
-      home: const MyHomePage(title: 'Hi, how\'s it going?'),
+      darkTheme: ThemeData(
+          colorScheme: ColorScheme.dark(),
+          useMaterial3: true,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero),
+                  minimumSize: const Size(300, 50)
+              )
+          )
+      ),
+      themeMode: _themeMode,
+      home: MyHomePage(title: 'Clippy', changeTheme: this.changeTheme, themeMode: _themeMode),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key, required this.title, required void Function(ThemeMode themeMode) changeTheme, required themeMode});
 
   final String title;
 
@@ -35,40 +73,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  int currentPage = 0;
+  static const pages = [Home(), UserProfile()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.errorContainer,
-        title: Text(widget.title),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        title: Text(widget.title, style: TextStyle()),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'How many times did I want to write to Amanda',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+      body: pages[currentPage],
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        shape: CircleBorder(),
+        onPressed: () => {
+          MyApp.of(context).changeTheme(
+              MyApp.of(context)._themeMode.name == 'dark' ? ThemeMode.light : ThemeMode.dark
+          ),
+        },
+        tooltip: 'Increment Counter',
+        child: const Icon(Icons.light_mode),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        clipBehavior: Clip.antiAlias,
+        shape: const CircularNotchedRectangle(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(onPressed: (){
+              setState(() {
+              currentPage = 0;
+            });
+              }, icon: Icon(Icons.home_filled)),
+            IconButton(onPressed: (){
+              setState(() {
+              currentPage = 1;
+            });
+              }, icon: Icon(Icons.person)),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      /*NavigationBar(
+        destinations: const [
+          NavigationDestination(
+              icon: Icon(Icons.home_filled),
+              label: 'Home',
+          ),
+          NavigationDestination(
+              icon: Icon(Icons.person),
+              label: 'Profile'
+          ),
+        ],
+        onDestinationSelected: (int index){
+          setState(() {
+            currentPage = index;
+          });
+        },
+        selectedIndex: currentPage,
+      ),*/
+
     );
   }
 }
