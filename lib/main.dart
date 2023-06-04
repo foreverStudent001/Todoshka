95427b94670d90b09fe8552e5ab354831d719a3f
Igor <igor-skromnik@yandex.ru>
2023-06-04 20:52:56 +0300

    add-block-logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
-import 'package:clippy/pages/home/home.dart';
-import 'package:clippy/pages/user/user_profile.dart';
+import 'package:clippy/src/global/application/global_blocs.dart';
+import 'package:clippy/src/global/application/global_states.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'src/home/presentation/screens/HomeWithBottomMenu.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-void test() {
-
 }
 
 class MyApp extends StatefulWidget {
@@ -21,119 +20,37 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // This widget is the root of your application.
-  ThemeMode _themeMode = ThemeMode.system;
-
-  void changeTheme(ThemeMode themeMode) {
-    setState(() {
-      _themeMode = themeMode;
-    });
-  }
-  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
-        useMaterial3: true,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.zero),
-                minimumSize: const Size(300, 50)
-            )
+    return BlocProvider(
+        create: (context) => GlobalBlocs(),
+        child: BlocBuilder<GlobalBlocs, GlobalStates>(
+          builder: (context, state) {
+            return MaterialApp(
+              title: 'Clippy',
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                  colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
+                  useMaterial3: true,
+                  elevatedButtonTheme: ElevatedButtonThemeData(
+                      style: ElevatedButton.styleFrom(
+                          shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero),
+                          minimumSize: const Size(300, 50)))),
+              darkTheme: ThemeData(
+                  colorScheme: const ColorScheme.dark(),
+                  useMaterial3: true,
+                  elevatedButtonTheme: ElevatedButtonThemeData(
+                      style: ElevatedButton.styleFrom(
+                          shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero),
+                          minimumSize: const Size(300, 50)))),
+              themeMode: state.themeMode,
+              home: const HomeWithBottomMenu(),
+              //MyHomePage(title: 'Clippy', changeTheme: this.changeTheme, themeMode: _themeMode),
+            );
+          },
         )
-      ),
-      darkTheme: ThemeData(
-          colorScheme: ColorScheme.dark(),
-          useMaterial3: true,
-          elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ElevatedButton.styleFrom(
-                  shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.zero),
-                  minimumSize: const Size(300, 50)
-              )
-          )
-      ),
-      themeMode: _themeMode,
-      home: MyHomePage(title: 'Clippy', changeTheme: this.changeTheme, themeMode: _themeMode),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title, required void Function(ThemeMode themeMode) changeTheme, required themeMode});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int currentPage = 0;
-  static const pages = [Home(), UserProfile()];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-        title: Text(widget.title, style: TextStyle()),
-      ),
-      body: pages[currentPage],
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        shape: CircleBorder(),
-        onPressed: () => {
-          MyApp.of(context).changeTheme(
-              MyApp.of(context)._themeMode.name == 'dark' ? ThemeMode.light : ThemeMode.dark
-          ),
-        },
-        tooltip: 'Increment Counter',
-        child: const Icon(Icons.light_mode),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        clipBehavior: Clip.antiAlias,
-        shape: const CircularNotchedRectangle(),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(onPressed: (){
-              setState(() {
-              currentPage = 0;
-            });
-              }, icon: Icon(Icons.home_filled)),
-            IconButton(onPressed: (){
-              setState(() {
-              currentPage = 1;
-            });
-              }, icon: Icon(Icons.person)),
-          ],
-        ),
-      ),
-      /*NavigationBar(
-        destinations: const [
-          NavigationDestination(
-              icon: Icon(Icons.home_filled),
-              label: 'Home',
-          ),
-          NavigationDestination(
-              icon: Icon(Icons.person),
-              label: 'Profile'
-          ),
-        ],
-        onDestinationSelected: (int index){
-          setState(() {
-            currentPage = index;
-          });
-        },
-        selectedIndex: currentPage,
-      ),*/
-
     );
   }
 }
