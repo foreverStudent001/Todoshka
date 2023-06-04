58f486f92767e3d81944bed132be046020f3f887
Igor <igor-skromnik@yandex.ru>
2023-06-04 20:53:16 +0300

    prepare-welcome-page

---
+++ lib/src/welcome/presentation/screens/welcom.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Welcome extends StatefulWidget {
+  const Welcome({super.key});
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Scaffold(
+        body: Container(
+          margin: EdgeInsets.only(top: 34.h),
+          width: 375.w,
+          child: Stack(
+            children: [
+              PageView(
+                children: [
+                  SizedBox(
+                    width: 345.w,
+                    height: 345.w,
+                    child: const Text('Image'),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
