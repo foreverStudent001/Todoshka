305a25c1a65fa5e454be9f11e3a864ccd1b0764e
Amanda L <dreampod13@gmail.com>
2023-12-11 14:20:52 -0500

    reusable custom text style

---
+++ lib/src/global_things/presentation/themes/custom_text_styles.dart
@@ -0,0 +1,11 @@
+import 'package:clippy/src/global_things/presentation/themes/ui_parameters.dart';
+import 'package:flutter/material.dart';
+
+//reusable
+
+TextStyle cartTitles(context) => TextStyle(
+    color:UIParameters.isDarkMode()?Theme.of(context).textTheme.bodyLarge!.color
+        :Theme.of(context).primaryColor,
+    fontSize:18,
+    fontWeight: FontWeight.bold
+);
