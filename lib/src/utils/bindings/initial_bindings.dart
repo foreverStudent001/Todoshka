5ae25ad7e0f7b053b5095c6e2093718fad87301a
Amanda L <dreampod13@gmail.com>
2023-12-10 20:30:37 -0500

    added two permissions to \main\AndroidManifest + initial binding

--- lib/src/utils/bindings/initial_bindings.dart
+++ lib/src/utils/bindings/initial_bindings.dart
@@ -1,3 +1,4 @@
+import 'package:clippy/src/global_things/application/services/firebase_storage_service.dart';
 import 'package:clippy/src/pages/presentation/controllers/auth_controller.dart';
 import'package:get/get.dart';
 import '../../global_things/presentation/controllers/theme_controller.dart';
@@ -8,5 +9,6 @@ class InitialBindings implements Bindings{
     //inject controllers
     Get.put(AuthController(), permanent: true); //use permanent to keep events in memory
     Get.put(ThemeController());
+    Get.put(FirebaseStorageService());
   }
 }
